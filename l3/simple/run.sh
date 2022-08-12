6ddc80fa92c951edbc1bb1df8d8c2677adfc6262
saarang <112001035@smail.iitpkd.ac.in>
2022-08-12 22:23:23 +0530

    removed unwanted files

--- l3/simple/run.sh
+++
@@ -1,8 +0,0 @@
-time python kakuro.py input0.txt output0.txt AC3
-time python kakuro.py input1.txt output1.txt AC3
-time python kakuro.py input2.txt output2.txt AC3
-time python kakuro.py input3.txt output3.txt AC3
-time python kakuro.py input4.txt output4.txt AC3
-time python kakuro.py input5.txt output5.txt AC3
-time python kakuro.py input6.txt output6.txt AC3
-time python kakuro.py input7.txt output7.txt AC3
